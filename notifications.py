868d732f5fb8e4b1e957381576dafc6257744333
Rodrigo Marliere <rmarliere@gmail.com>
2021-04-03 17:04:10 -0300

    more than one device on pushover

--- notifications.py
+++ notifications.py
@@ -30,17 +30,19 @@ class Notifications():
         return f"{self.config['exchange']['label']}: {self.config['label'].upper()} {up_or_down} @ {current_f} (%{change_f})"
 
     def _send_to_pushover(self):
-        conn = http.client.HTTPSConnection("api.pushover.net:443")
-        conn.request("POST", "/1/messages.json",
-        urllib.parse.urlencode({
-            "token": self.config['tokens']["pushover"]["token"],
-            "user": self.config['tokens']["pushover"]["user"],
-            "message": self.message,
-            "title": self.title,
-            "html": 1,
-        }), { "Content-type": "application/x-www-form-urlencoded" })
-        conn.getresponse()
-        logging.warning(f"Pushover sent: {self.message}")
+        devices = self.config['tokens']["pushover"]
+        for device in devices:
+            conn = http.client.HTTPSConnection("api.pushover.net:443")
+            conn.request("POST", "/1/messages.json",
+            urllib.parse.urlencode({
+                "token": device["token"],
+                "user": device["user"],
+                "message": self.message,
+                "title": self.title,
+                "html": 1,
+            }), { "Content-type": "application/x-www-form-urlencoded" })
+            conn.getresponse()
+            logging.warning(f"Pushover sent: {self.message}")
 
     def _send_to_slack(self):
         API_ENDPOINT = self.config['tokens']["slack"]["endpoint"]
